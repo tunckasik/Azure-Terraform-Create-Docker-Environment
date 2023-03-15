e7272ae106419328e35546250b7bc2c675591eea
Mehmet Ali TUNCKASIK <tunckasikali@gmail.com>
2023-03-15 00:59:57 -0400

    added tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+prefix          = "docker"
+location        = "eastus"
+admin_username  = "clouduser"
+ssh_key_rg      = "tf-state-rg"
+ssh_key_name    = "tf-state-key"
+ssh_private_key_path = "~/.ssh/"
--- variables.tf
+++ variables.tf
@@ -1,23 +1,17 @@
 variable "prefix" {
-  default = "docker"
 }
 
 variable "location" {
-  default = "eastus"
 }
 
 variable "admin_username" {
-  default = "clouduser"
 }
 
 variable "ssh_key_rg" {
-  default = "tf-state-rg"
 }
 
 variable "ssh_key_name" {
-  default = "tf-state-key"
 }
 
 variable "ssh_private_key_path" {
-  default = "~/.ssh/"
 }
